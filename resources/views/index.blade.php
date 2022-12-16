359b5e11a2d3d71b42238cb0cca75823f9b5feee
davidisnthere <wibisono.dav@gmail.com>
2022-12-16 19:04:32 +0700

    ngedit judul client

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -145,7 +145,7 @@
     <section id="clients" class="clients">
       <div class="container">
         <div class="section-title-client">
-          <h2>Apa itu RPL?</h2>
+          <h2>RPL Bekerja sama dengan</h2>
         </div>
 
         <div class="row no-gutters clients-wrap clearfix wow fadeInUp">
@@ -180,40 +180,6 @@
     </section><!-- End Clients Section -->
 
     <!-- ======= Counts Section ======= -->
-    <section id="counts" class="counts">
-      <div class="container">
-
-        <div class="text-center title">
-          <h3>What we have achieved so far</h3>
-          <p>Iusto et labore modi qui sapiente xpedita tempora et aut non ipsum consequatur illo.</p>
-        </div>
-
-        <div class="row counters position-relative">
-
-          <div class="col-lg-3 col-6 text-center">
-            <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
-            <p>Clients</p>
-          </div>
-
-          <div class="col-lg-3 col-6 text-center">
-            <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
-            <p>Projects</p>
-          </div>
-
-          <div class="col-lg-3 col-6 text-center">
-            <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1" class="purecounter"></span>
-            <p>Hours Of Support</p>
-          </div>
-
-          <div class="col-lg-3 col-6 text-center">
-            <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1" class="purecounter"></span>
-            <p>Hard Workers</p>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Counts Section -->
 
     <!-- ======= Portfolio Section ======= -->
     <section id="portfolio" class="portfolio">
