f551c28ef6fd5ff6ddb88abaea6b54192959d51c
rawhx_x <achmadhasbil24@gmail.com>
2022-12-19 14:16:13 +0700

    database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory()->create();
+        \App\Models\Kontak::create([
+            'deskripsi' => '',
+            'email' => 'mail@smkn4malang.sch.id',
+            'telepon' => '(0341) 353798',
+        ]);
     }
 }
